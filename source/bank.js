ad96bbe0871fb242145aa90f2aad779496500464
User <user.push@hackerrank.workspace>
2020-08-03 18:34:04 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- source/bank.js
+++ source/bank.js
@@ -1,9 +1,21 @@
+const data = require("./data.js")
+module.imports = data;
+
 class Account{
-    
+
 }
 
 class SavingsAccount{
-    
+
+  constructor(name,acctID, mob, bal){
+
+
+  }
+
+    withdraw = (x) => {
+
+          return this.bal-x
+  }
 }
 
 module.exports = {
